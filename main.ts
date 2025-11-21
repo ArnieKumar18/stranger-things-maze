3cfebea653cc3582d9d5992524294370ae5abc88
ArnieKumar18 <arnav.kumar@panam.ed.cr>
2025-11-21 09:09:19 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+    game.over(false)
+})
+info.startCountdown(10)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
+scene.cameraFollowSprite(mySprite)
